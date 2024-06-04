f46e8d63cdc14ede7a25fc5fc71974a46f712796
Vijaykrishnamoorthi <vijaykrishnamoorthi98@gmail.com>
2024-06-04 06:27:52 +0530

    junit task

--- src/test/java/in/redbus/BusBooking.java
+++ src/test/java/in/redbus/BusBooking.java
@@ -8,6 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -30,13 +31,13 @@ public class BusBooking {
 		}
 	@Test
 	public void method2() {
-	driver.findElement(By.xpath("//input[@id='src']")).sendKeys("Thiruvannamalai");
-	driver.findElement(By.xpath("//text[text()='Thiruvannamalai']")).click();
+	driver.findElement(By.xpath("//input[@id='src']")).sendKeys("Chennai");
+	driver.findElement(By.xpath("//text[text()='Chennai']")).click();
 	}
 	@Test
 	public void method3() {
-	driver.findElement(By.xpath("//input[@id='dest']")).sendKeys("Bangalore");
-	driver.findElement(By.xpath("//text[text()='Bangalore']")).click();
+	driver.findElement(By.xpath("//input[@id='dest']")).sendKeys("Trichy");
+	driver.findElement(By.xpath("//text[text()='Trichy']")).click();
 		}
 	@Test
 	public void method4() {
@@ -53,36 +54,61 @@ public class BusBooking {
 	}
 	@Test
 	public void method7() {
-		List<WebElement> gt=driver.findElements(By.xpath("//div[@class='bus-type f-12 m-top-16 l-color evBus']"));
-		List<WebElement> gg=driver.findElements(By.xpath("//div[@class='dp-time f-19 d-color f-bold']"));
-		List<WebElement> gh=driver.findElements(By.xpath("//div[@class='dur l-color lh-24']"));////span[@class='f-19 f-bold']
-		List<WebElement> gz=driver.findElements(By.xpath("//div[@class='bp-time f-19 d-color disp-Inline']"));
-		List<WebElement> gy=driver.findElements(By.xpath("//span[@class='f-19 f-bold']"));
-		System.out.println("Available Buses:");
-		for(WebElement wh:gt) {
-			System.out.println(wh.getText());	
-		}
-		System.out.println("Departure Time:");
-		for(WebElement wh:gg) {
-			System.out.println(wh.getText());
-		}
-		System.out.println("Duration:");
-		for(WebElement wh:gh) {
-			System.out.println(wh.getText());
-		}
-		System.out.println("Arrival Time:");
-		for(WebElement wh:gz) {
-			System.out.println(wh.getText());
-		}
-		
-		System.out.println("BusFare:");
-		for(WebElement wh:gy) {
-			System.out.println(wh.getText());
-		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		List<WebElement> a= driver.findElements(By.xpath("//div[contains(@class,'travel')]"));
+		List<WebElement> b = driver.findElements(By.xpath("//div[contains(@class,'dp-time')]"));
+		List<WebElement> c = driver.findElements(By.xpath("//div[contains(@class,'bp-time')]"));
+		List<WebElement> d = driver.findElements(By.xpath("//div[contains(@class,'fare d-block')]"));
+		for (int i = 0; i < a.size(); i++) {
+			if (i == a.size()-1) {
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("window.scrollTo(0,document.body.scrollHeight)",a.get(i));
+		    a=driver.findElements(By.xpath("//div[contains(@class,'travel')]"));
+			b=driver.findElements(By.xpath("//div[contains(@class,'dp-time')]"));
+			c=driver.findElements(By.xpath("//div[contains(@class,'bp-time')]"));
+			d=driver.findElements(By.xpath("//div[contains(@class,'fare d-block')]"));
+			}else {
+			String e = a.get(i).getText();
+			String f = b.get(i).getText();
+			String g = c.get(i).getText();
+			String h = d.get(i).getText();
+			System.out.println("Bus Name: "+e+"Departure Time: "+f+"Arrival Time: "+g+"Fare rate: "+h);
+			}
 	}
-
-	@AfterClass
-	public static void method8() {
-		driver.close();
-	}	
-}
+	}
+	}
+//		List<WebElement> ab=driver.findElements(By.xpath("//div[@class='travels lh-24 f-bold d-color']"));
+//		List<WebElement>cd=driver.findElements(By.xpath("//div[@class='dp-time f-19 d-color f-bold']"));
+//		List<WebElement>ef=driver.findElements(By.xpath("//div[@class='bp-time f-19 d-color disp-Inline']"));
+//		List<WebElement> gh=driver.findElements(By.xpath("//div[@class='fare d-block']"));
+//
+//		for(int i=0;i<ab.size();i++) {
+//			if(i==ab.size()-1) {
+////				JavacriptExecutor js = (JavascriptExecutor)driver;
+////				js.executeScript("window.scrollTo(0,docment.body.scrollHeight)",ab.get(i));
+//				JavascriptExecutor js = (JavascriptExecutor) driver;
+//				js.executeScript("window.scrollTo(0,document.body.scrollHeight)",ab.get(i));
+//			    ab=driver.findElements(By.xpath("//div[@class='travels lh-24 f-bold d-color']"));
+//				cd=driver.findElements(By.xpath("//div[@class='dp-time f-19 d-color f-bold']"));
+//				ef=driver.findElements(By.xpath("//div[@class='bp-time f-19 d-color disp-Inline']"));
+//				gh=driver.findElements(By.xpath("//div[@class='fare d-block']"));
+//				System.out.println(ab.size());
+//				
+//			}
+//
+//			else {
+//				String bus =ab.get(i).getText();
+//				String dp =cd.get(i).getText();
+//				String ap=ef.get(i).getText();
+//				String fare =gh.get(i).getText();
+//				System.out.println("Bus Name : "+ bus+" pickup Time "+dp +" Arival Time :" +ap+" Bus Fare :"+fare);
+//			}
+//			
+//		}}
+//		
+//	
+//	@AfterClass
+//	public static void method8() {
+//		driver.close();
+//	}	
+//}
